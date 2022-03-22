b2bfb6f7fc40e62373628c1f70eca33190f39c93
Matheus Rodrigues <rodmatheus96@gmail.com>
2022-03-22 14:19:42 -0300

    :sparkles: implementa resource server do keycloak.

---
+++ src/main/java/br/com/ponto/registro/core/security/ResourceServerConfig.java
@@ -0,0 +1,59 @@
+package br.com.ponto.registro.core.security;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import br.com.ponto.registro.domain.exception.AcessoNaoAutorizadoException;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().csrf().disable().cors().and().oauth2ResourceServer()
+				.jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+	}
+
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+
+			var authorities = Optional
+					.of(jwt.getClaimAsStringList("roles"))
+						.orElseThrow(
+								() -> new AcessoNaoAutorizadoException("Você não possui autorização para acessar este sistema!"));
+
+			var scopesAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+			Collection<GrantedAuthority> grantedAuthorities = scopesAuthoritiesConverter.convert(jwt);
+
+			grantedAuthorities.addAll(authorities.stream()
+					.map(SimpleGrantedAuthority::new)
+					.collect(Collectors.toList()));
+
+			return grantedAuthorities;
+		});
+
+		return jwtAuthenticationConverter;
+	}
+
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+}
