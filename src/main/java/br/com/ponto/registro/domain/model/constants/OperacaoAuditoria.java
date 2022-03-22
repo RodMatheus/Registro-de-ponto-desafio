184eda542b28bf2a09523062c08915f138d6a19a
Matheus Rodrigues <rodmatheus96@gmail.com>
2022-03-22 17:07:35 -0300

    :sparkles: implementa a criação de logs operacionais do sistema. (#7)

---
+++ src/main/java/br/com/ponto/registro/domain/model/constants/OperacaoAuditoria.java
@@ -0,0 +1,33 @@
+package br.com.ponto.registro.domain.model.constants;
+
+import lombok.Getter;
+import java.util.Arrays;
+
+import br.com.ponto.registro.domain.exception.ValidacaoException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum OperacaoAuditoria {
+
+	INCLUSAO(1, "Inclusão");
+	
+	private final Integer codigo;
+    private final String nome;
+    
+    public static OperacaoAuditoria byNome(String nome) {
+        return Arrays
+                .stream(values())
+                .filter(tipo -> tipo.nome.equalsIgnoreCase(nome))
+                .findAny()
+                .orElseThrow(() -> new ValidacaoException("O nome informado é inválido!"));
+    }
+    
+    public static OperacaoAuditoria byCodigo(Integer codigo) {
+        return Arrays
+                .stream(values())
+                .filter(tipo -> tipo.codigo.equals(codigo))
+                .findAny()
+                .orElseThrow(() -> new ValidacaoException("O código informado é inválido!"));
+    }
+}
