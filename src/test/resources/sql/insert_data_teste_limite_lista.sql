3ca6988d819653c86c6bbfcc7e3b2be1e36cea26
Matheus Rodrigues <rodmatheus96@gmail.com>
2022-04-04 14:50:56 -0300

    :recycle: ajusta testes de integração com novo usuário. (#26)

--- src/test/resources/sql/insert_data_teste_limite_lista.sql
+++ src/test/resources/sql/insert_data_teste_limite_lista.sql
@@ -1,4 +1,4 @@
-INSERT INTO ponto.ponto_eletronico (usuario_logado, horario, descricao) VALUES('rodmatheus96@gmail.com', '2022-03-18T09:30:25', 'teste 1');
-INSERT INTO ponto.ponto_eletronico (usuario_logado, horario, descricao) VALUES('rodmatheus96@gmail.com', '2022-03-18T12:30:25', 'teste 2');
-INSERT INTO ponto.ponto_eletronico (usuario_logado, horario, descricao) VALUES('rodmatheus96@gmail.com', '2022-03-18T13:30:25', 'teste 3');
-INSERT INTO ponto.ponto_eletronico (usuario_logado, horario, descricao) VALUES('rodmatheus96@gmail.com', '2022-03-18T18:30:25', 'teste 4');
+INSERT INTO ponto.ponto_eletronico (usuario_logado, horario, descricao) VALUES('teste@teste.com', '2022-03-18T09:30:25', 'teste 1');
+INSERT INTO ponto.ponto_eletronico (usuario_logado, horario, descricao) VALUES('teste@teste.com', '2022-03-18T12:30:25', 'teste 2');
+INSERT INTO ponto.ponto_eletronico (usuario_logado, horario, descricao) VALUES('teste@teste.com', '2022-03-18T13:30:25', 'teste 3');
+INSERT INTO ponto.ponto_eletronico (usuario_logado, horario, descricao) VALUES('teste@teste.com', '2022-03-18T18:30:25', 'teste 4');
